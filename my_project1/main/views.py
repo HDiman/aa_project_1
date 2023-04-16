6b8b18b50d882f74703ce93491c3c2b06998d1bc
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-04-16 19:37:49 +0300

    aa_project_1 ver 2.04 (Добавил templates)

--- my_project1/main/views.py
+++ my_project1/main/views.py
@@ -5,11 +5,11 @@ def index(request):
     data = {
         'title': 'Главная страница',
     }
-    return render(request, 'main/index.html', data)
+    return render(request, 'news/index.html', data)
 
 def about(request):
     data = {
         'info': 'Информация о нашей компании',
     }
-    return render(request, 'main/about.html', data)
+    return render(request, 'news/about.html', data)
 
--- my_project1/news/views.py
+++ my_project1/news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def news_home(request):
+    data = {
+        'title': 'Yahoo News',
+    }
+    return render(request, 'news/index.html', data)
