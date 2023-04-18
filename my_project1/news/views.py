409ff93d7134ffd6a04cffbace3d4e682247dec4
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-04-19 00:57:34 +0300

    Создал возможность сохранять данные в базе данных и выводить на странице новости

--- my_project1/news/views.py
+++ my_project1/news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 
 # Create your views here.
 def news_home(request):
@@ -7,4 +8,18 @@ def news_home(request):
     return render(request, 'news/news_home.html', {'news': news})
 
 def create(request):
-    return render(request, 'news/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = ArticlesForm()
+    data = {
+        'form': form,
+        'error': error,
+    }
+    return render(request, 'news/create.html', data)
