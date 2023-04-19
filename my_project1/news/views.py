ab2583839b8f51ce9383c09d0fd2e03e918c9e70
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-04-19 11:20:40 +0300

    Добавил возможность динамической страницы

--- my_project1/news/views.py
+++ my_project1/news/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
+
 
 # Create your views here.
 def news_home(request):
     news = Articles.objects.all()
     return render(request, 'news/news_home.html', {'news': news})
+
+
+# Создание нового класса для динамической страницы
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
 
 def create(request):
     error = ''
